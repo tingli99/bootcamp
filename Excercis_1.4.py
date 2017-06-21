190e5190873414a57d7c6c511d9a78470ed415f2
tingli99 <tingli99@caltech.edu>
2017-06-20 20:27:05 -0700

    some renaming

--- Excercis_1.4.py
+++
@@ -1,8 +0,0 @@
-def longest_common_substring(seq1,seq2)
-    """return the longest_common_substring between two sequences"""
-    # Initialize the longest_common_substring
-    longest_common_substring = ''
-
-    for base in longest_common_substring(seq1,seq2):
-        if Aa in seq1, find Aa in seq2
-            
