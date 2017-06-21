2c00f4a61a78d27ebe09c7dfc601c6b3eddf3752
tingli99 <tingli99@caltech.edu>
2017-06-20 20:13:09 -0700

    Added three exercises files.

---
+++ Excercis_1.4.py
@@ -0,0 +1,8 @@
+def longest_common_substring(seq1,seq2)
+    """return the longest_common_substring between two sequences"""
+    # Initialize the longest_common_substring
+    longest_common_substring = ''
+
+    for base in longest_common_substring(seq1,seq2):
+        if Aa in seq1, find Aa in seq2
+            
